9b4b61307215bafc6c958de86770c3f36915bfbf
GreyyDaze <117322834+GreyyDaze@users.noreply.github.com>
2024-02-18 08:29:59 +0500

    home page design change

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -6,6 +6,7 @@ import TaskCard from "./TaskCard";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import { Card } from "react-bootstrap";
 
 const Tasks = JSON.parse(localStorage.getItem("TaskList"));
 console.log("Tasks", Tasks);
@@ -66,8 +67,8 @@ const TodoList = ({ mode, setMode }) => {
           </Button>
         </div>
       </div>
-      <div className="mt-5">
-        <Container className="d-flex justify-content-end mb-5 ">
+      <div className="mt-5 content">
+        <Container className="d-flex justify-content-end mb-5">
           {mode === "light" ? (
             <a onClick={() => setMode("dark")}>
               <svg
@@ -98,21 +99,41 @@ const TodoList = ({ mode, setMode }) => {
         </Container>
         <Container>
           <Row>
-            {taskList.map((task, index) => (
-              <Col xs={12} sm={6} md={4} lg={3} key={index}>
-                <TaskCard
-                  task={task}
-                  index={index}
-                  deleteTask={deleteTask}
-                  editTask={editTask}
-                  markComplete={markComplete}
-                />
-              </Col>
-            ))}
+            {taskList &&
+              taskList.map((task, index) => (
+                <Col xs={12} sm={6} md={4} lg={3} key={index}>
+                  <TaskCard
+                    task={task}
+                    index={index}
+                    deleteTask={deleteTask}
+                    editTask={editTask}
+                    markComplete={markComplete}
+                  />
+                </Col>
+              ))}
+            <div className="mt-4 mb-5 d-flex justify-content-center">
+              <Card>
+                <Card.Body className="text-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="35"
+                    height="35"
+                    fill="currentColor"
+                    className="bi bi-emoji-smile-fill mb-3"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16M7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5M4.285 9.567a.5.5 0 0 1 .683.183A3.5 3.5 0 0 0 8 11.5a3.5 3.5 0 0 0 3.032-1.75.5.5 0 1 1 .866.5A4.5 4.5 0 0 1 8 12.5a4.5 4.5 0 0 1-3.898-2.25.5.5 0 0 1 .183-.683M10 8c-.552 0-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5S10.552 8 10 8" />
+                  </svg>
+                  <Card.Title>
+                    No Task Created Yet. Create Task to see it here
+                  </Card.Title>
+                </Card.Body>
+              </Card>
+            </div>
           </Row>
         </Container>
       </div>
-      <footer className="mt-5 bg-dark text-white pt-3 pb-2 ">
+      <footer className="mt-5 bg-dark text-white pt-3 pb-2 position-relative bottom-0">
         <p className="text-center font-weight-bold">
           Made by Aminah &copy; 2024
         </p>
